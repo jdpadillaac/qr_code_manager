87cbb77c9cf57f610b140c4b083e46f7334b46c2
Jonatan Padilla <igjdpadillac@gmail.com>
2025-02-22 20:00:13 -0500

    refactor: update app module to use qr_code_manager_auth and clean up tests

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:qr_code_manager/main.dart';
+import 'package:qr_code_manager_auth/qr_code_manager_auth.dart';
 
 final class AppModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.child('/', child: (_) => const MyHomePage(title: 'Some'));
+    r.module('/', module: AuthModule());
   }
 }
