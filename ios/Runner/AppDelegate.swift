ba95ea90bd86a64f44b8d6b3f2db7b799058797c
Jonatan Padilla <igjdpadillac@gmail.com>
2025-02-23 12:33:03 -0500

    feat: integrate biometric authentication using FaceID; update AppDelegate and Info.plist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import LocalAuthentication
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,36 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let flutterCtr = window?.rootViewController as? FlutterViewController
+      let channel = FlutterMethodChannel(name: "biometic_auth", binaryMessenger: (flutterCtr?.binaryMessenger)! )
+        channel.setMethodCallHandler { (call, result) in
+            if call.method == "validateBiometric" {
+                self.auth(result: result)
+            }
+        }
+        
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func auth(result: @escaping FlutterResult ) -> Void {
+        var laContextError: NSError?
+        let laContext = LAContext()
+        if  laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &laContextError) {
+            laContext.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: "Autenticate con FaceID")
+            { authSuccess,authErr in
+                if (authSuccess) {
+                    result(true)
+                } else {
+                    result(false)
+                }
+            }
+            
+        } else {
+            result(false)
+        }
+        
+    }
 }
